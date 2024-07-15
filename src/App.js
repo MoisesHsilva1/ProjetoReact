5594c305e32f70593dc20d72a2e09c73745ab694
Moises Henrique <moihgm67@gmail.com>
2024-07-15 19:09:34 -0300

    Criando um Html

--- src/App.js
+++ src/App.js
@@ -1,25 +1,3 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edite save para recarregar
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function app() {
+    
 }
-
-export default App;
